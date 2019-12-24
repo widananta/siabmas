0c9a61686133edb47fe811cd3d469e18a0e355de
Ringgia Widananta Fikar <r.widananta.f@gmail.com>
2019-12-24 11:36:02 +0700

    fixing a bug

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -118,7 +118,7 @@
                                 <div class="row">
                                     <div class="col">
                                         <h5 class="card-title text-uppercase text-muted mb-0">Jumlah Matkul diampu</h5>
-                                        <span class="h2 font-weight-bold mb-0">{{$mahasiswa}}</span>
+                                        <span class="h2 font-weight-bold mb-0">{{$pengampu}}</span>
                                     </div>
                                     <div class="col-auto">
                                         <div class="icon icon-shape bg-info text-white rounded-circle shadow">
